6aaf54de5c0e65152edfdb0215a4e4b87f4e3f47
VlasyaPlay <alexvlasov9301@gmail.com>
2024-06-27 13:46:42 +0700

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+course_name = 'Python'
+number_of_tasks = 12
+number_of_hours_spend = 1.5
+time_for_the_task = number_of_hours_spend / number_of_tasks
+print('Курс:', course_name,',', 'Всего задач:', number_of_tasks,',', 'Затрачено часов:', number_of_hours_spend,',', 'Среднее время выполнения:', time_for_the_task)
