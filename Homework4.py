17cfaf449e4fa7cfea453d13958394603d9f1469
VlasyaPlay <alexvlasov9301@gmail.com>
2024-06-29 20:50:49 +0700

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,10 @@
+# организация программы
+my_string=input('Введите текст: ',)
+print(len(my_string))
+
+# работа с методами
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
